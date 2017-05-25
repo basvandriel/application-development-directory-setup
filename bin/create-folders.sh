27b086c76163895ce90334227d18d1c60e1cc696
Bas van Driel <basvandriel94@gmail.com>
2017-05-25 16:06:28 +0200

    ADded todos

--- bin/create-folders.sh
+++ bin/create-folders.sh
@@ -1,1 +1,11 @@
 #!/bin/bash
+
+## TODO Find the home directory of the current user
+## TODO Prompt for data where to store all the folders
+## TODO Create the Web_development folder if it doesn't yet exist
+## TODO Create the Desktop_application_development folder if it doesn't yet exist
+## TODO Create the Other_application_development folder if it doesn't yet exist
+## TODO Create the Mobile_application_development folder if it doesn't yet exist
+
+# End of the application
+read -p "Press enter to continue"
