91ca49efee78196499e741de9b1559693e6988d6
Bas van Driel <basvandriel94@gmail.com>
2017-07-12 00:49:24 +0200

    Set folder names

--- bin/create-folders.cmd
+++ bin/create-folders.cmd
@@ -1,5 +1,8 @@
 @echo off
 
+:: Set the folder names we want to create
+SET FOLDER_NAMES=Web_development Desktop_application_development Mobile_application_development Other_application_development
+
 :: Welcome the user
 echo Welcome to the application development directory setup
 echo - Created by Bas van Driel
@@ -9,6 +12,12 @@ SET FOLDERS_INSTALL_LOCATIONS=%UserProfile%
 echo The install location is standard set to the user profile,
 echo (%FOLDERS_INSTALL_LOCATIONS%) would you like to change that? (Y/N)
 echo.
+
+:: TODO Prompt for data where to store all the folders
+:: TODO Create the Web_development folder if it doesn't yet exist
+:: TODO Create the Desktop_application_development folder if it doesn't yet exist
+:: TODO Create the Other_application_development folder if it doesn't yet exist
+:: TODO Create the Mobile_application_development folder if it doesn't yet exist
 
 :: End the application
 pause
